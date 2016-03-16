8d6af58f99c2e4afaf4a90904232e92f2efc3669
anna hunter <annfmonty@gmail.com>
2016-03-16 07:58:00 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+//listen for click on button
+
+var btn = document.querySelector("button");
+console.log(btn);
+btn.addEventListener("click", loader);
+var clickCounter = 0;
+var loader = function(){
+	
+};
+console.log();
